ecf98694fa8ef0583fdf1a27c9d0939d166d56b8
Russel Osiemo <russelosiemo@gmail.com>
2022-02-22 16:45:49 +0300

    work1

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <h1>Hello World Food Organization</h1>
 }
 
 export default MyApp
