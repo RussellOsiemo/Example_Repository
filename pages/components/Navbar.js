f14a3c3c70b117cd2bf65c3ec50f4bb6ad55566d
Russel Osiemo <russelosiemo@gmail.com>
2022-02-23 10:28:03 +0300

    starter template

---
+++ pages/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+export const Navbar = () => {
+  return (
+    <div>
+    <div className='container mt-0  w-full bg-slate-300  h-24 rounded-md'>
+
+    </div>
+
+    </div>
+  )
+}
+export default Navbar;
